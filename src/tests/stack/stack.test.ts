b484c4394aad560b2cc88ee240c8aaf4c6129e1d
PilarczykM <marcinpilarczyk@me.com>
2022-05-03 15:21:27 +0200

    Add basic test for stack.

---
+++ src/tests/stack/stack.test.ts
@@ -0,0 +1,34 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { Stack } from './stack';
+
+describe('My Stack', () => {
+  let stack: Stack<string>;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('is created empty.', () => {
+    expect(stack.size()).toBe(0);
+  });
+
+  it('can push to the top.', () => {
+    const item = '🍧';
+
+    stack.push(item);
+
+    expect(stack.size()).toBe(1);
+    expect(stack.peek()).toBe(item);
+  });
+
+  it('can pop off', () => {
+    const item = '🍧';
+
+    stack.push(item);
+    const popValue = stack.pop();
+
+    expect(item).toBe(popValue);
+    expect(stack.size()).toBe(0);
+    expect(stack.pop()).toBe(undefined);
+  });
+});
