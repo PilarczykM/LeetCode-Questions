05ef594492235107d5084b89763242d48fcbc0b4
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-02-07 15:55:47 +0100

    Add

--- src/palindrome-number/palindrome.spec.ts
+++ src/palindrome-number/palindrome.spec.ts
@@ -4,7 +4,7 @@ import { isPalindrome } from './palindrome';
 function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 describe('Palindrome', () => {
