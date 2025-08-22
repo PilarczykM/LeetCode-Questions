0ad408a5264cf5a1c79f5246ac5532c987f9dc58
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-08-22 12:21:37 +0200

    Update analize

--- src/two_sum/two_sum.py
+++ src/two_sum/two_sum.py
@@ -1,5 +1,5 @@
 class TwoSum:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:  # 0(n^2)
+    def twoSum(self, nums: list[int], target: int) -> list[int]:  # 0(n^2) 18.63MB
         for idx, x in enumerate(nums):
             for jdx, y in enumerate(nums):
                 if idx >= jdx:
@@ -19,12 +19,13 @@ class TwoSum:
 
         raise ValueError(f"{target} not found in list")
 
-    def twoSum_3(self, nums: list[int], target: int) -> list[int]:
-        seen: dict[int, int] = {}
-        for idx, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], idx]
-            seen[num] = idx
-
+    def twoSum_3(self, nums: list[int], target: int) -> list[int]:  # O(n) 19.17MB
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+        # If no valid pair is found, return an empty list
         raise ValueError(f"{target} not found in list")
